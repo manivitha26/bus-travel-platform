95333839ca7f90fd88cc09e7a0379bc072d1fedb
manivitha26 <machanamanivitha@gmail.com>
2026-03-20 20:37:41 +0530

    Create SearchBuses component

---
+++ frontend/src/components/SearchBuses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBuses = () => {
+    return (
+        <div>
+            <h1>Search Buses</h1>
+            {/* Add your search form or filters here */}
+        </div>
+    );
+};
+
+export default SearchBuses;
